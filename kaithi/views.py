d7f5d23fbb3ffd93cd094ee032b218bc95fc3e23
i'm Devops Engineer <thekallurisantoshreddy@gmail.com>
2023-03-16 19:32:24 +0530

    first commit

---
+++ kaithi/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def vikram(request):
+    return render(request,'vikram.html')
+
+
+
+def amar(request):
+    return render(request,'amar.html')
